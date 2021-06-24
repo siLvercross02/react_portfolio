6b588d084fe40d5fee0d4bf4f078622d43e217dd
Joemari H. Supan <killjoe294@gmail.com>
2021-06-24 22:18:23 +0800

    Update portfolio

---
+++ build/precache-manifest.d6bd33f4b9977d1ef1ce50bd87b65fe1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d0b095c4503a3ba3fc9d56442233b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb5c96c910428ff6daa7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "af25f5fcaa0eb171415e",
+    "url": "/static/css/main.b84ad5e1.chunk.css"
+  },
+  {
+    "revision": "cb5c96c910428ff6daa7",
+    "url": "/static/js/2.c3bcd3ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c3bcd3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af25f5fcaa0eb171415e",
+    "url": "/static/js/main.589d851d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c7a7fec413959c2bf219f4680c359550",
+    "url": "/static/media/Affinity-portfolio.c7a7fec4.PNG"
+  },
+  {
+    "revision": "e4197443748c99f03bc8917bb4756383",
+    "url": "/static/media/about-2.e4197443.jpg"
+  },
+  {
+    "revision": "2a972cdcb0cc35cafdc94712ae721e76",
+    "url": "/static/media/amazon.2a972cdc.PNG"
+  },
+  {
+    "revision": "461ce4d9a25afd490565e1a8cfd25e67",
+    "url": "/static/media/bg.461ce4d9.jpg"
+  },
+  {
+    "revision": "4a9f45720bb1ecdf3ba628b9517cd540",
+    "url": "/static/media/bootstrap.4a9f4572.png"
+  },
+  {
+    "revision": "e3dc9a7efb006d9c56fbe3de84e30159",
+    "url": "/static/media/communifi.e3dc9a7e.png"
+  },
+  {
+    "revision": "6886d0e7f51c9940aa9763aa96ab3bba",
+    "url": "/static/media/communitalk.6886d0e7.png"
+  },
+  {
+    "revision": "b342724c4dd9ae42a9b7bf5c0a778ec1",
+    "url": "/static/media/css-64.b342724c.png"
+  },
+  {
+    "revision": "c894a81f8e70aa04c63b57a58ff6585b",
+    "url": "/static/media/fb-logo-circle.c894a81f.png"
+  },
+  {
+    "revision": "d31355c9bd6c5a164cdc310abd7e1c36",
+    "url": "/static/media/github-logo-new.d31355c9.png"
+  },
+  {
+    "revision": "ca3d3390efd24e04ec213930b642cc3c",
+    "url": "/static/media/gmail-logo-new.ca3d3390.png"
+  },
+  {
+    "revision": "d1bd75f16cafce0330900bb9a6142cdf",
+    "url": "/static/media/html-64.d1bd75f1.png"
+  },
+  {
+    "revision": "cbd03c6c448f5d427bc0693f825bc5fe",
+    "url": "/static/media/js-64.cbd03c6c.png"
+  },
+  {
+    "revision": "16828a69a85cdf6a308bae6d4c3cbc82",
+    "url": "/static/media/klickstreamzone.16828a69.png"
+  },
+  {
+    "revision": "b404967dbfc806ad21c5beaa1f55f513",
+    "url": "/static/media/klickvpn.b404967d.png"
+  },
+  {
+    "revision": "1e444f45c57add7bd46f671ce0c9d776",
+    "url": "/static/media/linkedin-logo-new.1e444f45.png"
+  },
+  {
+    "revision": "f32034d4962e23745b4b72a5e7490869",
+    "url": "/static/media/netflix.f32034d4.PNG"
+  },
+  {
+    "revision": "0762b0a86ada84c59df2acba57cd8737",
+    "url": "/static/media/php-64.0762b0a8.png"
+  },
+  {
+    "revision": "e45ba96f4b66cc3432a80db6d28cf165",
+    "url": "/static/media/profile.e45ba96f.jpg"
+  },
+  {
+    "revision": "95d4971a1bf67a0b571360f9bfe92742",
+    "url": "/static/media/ps-64.95d4971a.png"
+  },
+  {
+    "revision": "9fc39788219a7cd496c1e42e1bfdc329",
+    "url": "/static/media/reactjs-64.9fc39788.png"
+  },
+  {
+    "revision": "2c5e9e0432b9b97eb745bb730f692c61",
+    "url": "/static/media/univ.2c5e9e04.png"
+  },
+  {
+    "revision": "3d3142496b8e51f5950c893af109735d",
+    "url": "/static/media/working-exp.3d314249.png"
+  },
+  {
+    "revision": "895fb0b8a50df27f7ef9e2656b825688",
+    "url": "/static/media/xd-64.895fb0b8.png"
+  }
+]);
