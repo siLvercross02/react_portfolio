a94fda9c88481c3d5400959cc28fb7683c58b7a0
Joemari Supan <joemari.supan18@gmail.com>
2022-10-21 20:22:17 +0800

    Update Skills

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import {
-  BrowserRouter as Router
-} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Components
-import Header from './components/Header/Header';
-import Banner from './components/Banner/Banner';
-import About from './components/About/About';
-import Working from './components/About/WorkingExp';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+import About from "./components/About/About";
+import Working from "./components/About/WorkingExp";
+import Skills from "./components/Skills/NewSkills";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Router>
-        <Header />
-        <Banner />
-        <About />
-        <Working />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
+      <Header />
+      <Banner />
+      <About />
+      <Working />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </Router>
   );
 }
